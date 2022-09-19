7a4e78a959575be1b429a9cf2fb2c3413390fdb1
samuelkufre <betwithchief@gmail.com>
2022-09-19 13:42:01 -0700

    0x05-pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ * @n: reference to n
+ */
+
+
+void reset_to_98(int *n);
+
+int main(void)
+{
+	int n = 402;
+
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
+/**
+ * reset_to_98 - return n
+ * Return:n
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
