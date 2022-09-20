0fab7eda44630a0455894e035ed00ea41a3f91c6
samuelkufre <betwithchief@gmail.com>
2022-09-19 20:31:50 -0700

    chore: print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * print_rev - print string in reverse
+ *@s: string to reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(*s); i > 0; i--)
+	{
+		putchar(*s);
+		s--;
+	}
+	putchar('\n');
+}
+
