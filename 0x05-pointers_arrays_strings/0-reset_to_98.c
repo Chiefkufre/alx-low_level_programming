49d0ffee9677f90b5b7087caa88a898a2ecdfaa0
samuelkufre <betwithchief@gmail.com>
2022-09-19 18:58:20 -0700

    chore: test this file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,26 +1,6 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- *
- * @n: reference to n
- */
-
-
-void reset_to_98(int *n);
-
-int main(void)
-{
-	int n = 402;
-
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
-/**
  * reset_to_98 - return n
  * Return:n
  *
