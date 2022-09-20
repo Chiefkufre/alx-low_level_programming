7a3ec976bf7443a5b8da0e0b4b06ad44014213b0
samuelkufre <betwithchief@gmail.com>
2022-09-19 21:25:08 -0700

    chore: first attempt

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,23 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * _strlen = return string lenght
+ *@s: string
+* Return: string length
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
 
 /**
  * put2 - print every second character of a string
@@ -12,7 +29,7 @@ void put2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0' && i < strlen(str); i += 2)
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 		putchar(str[i]);
 	putchar('\n');
 }
