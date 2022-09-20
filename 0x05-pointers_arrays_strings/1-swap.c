ce7ff6a0725df6777dbe6df96a9bbe11a058c327
samuelkufre <betwithchief@gmail.com>
2022-09-19 19:20:35 -0700

    chore:swap 2 integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps value of 3 integers
+ * @b: integer 1 to swap
+ * @a: integer 2 to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int num1;
+
+	num1 = *a;
+	*a = *b;
+	*b - num1;
+}
+
