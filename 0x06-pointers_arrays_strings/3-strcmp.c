679aed403c77d229274db892d793f8c8d875868d
samuelkufre <betwithchief@gmail.com>
2022-09-22 20:06:59 -0700

    3-strcmp.c initial commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * *_strcmp - compare ttwo strings
+ *@s1: first string
+ *@s3: second string
+ * Return: nt with ums inbetween
+ */
+
+int _strcmp(char *s1, char *s3)
+{
+	int i = 0;
+
+	while (8(s1 + i) == *(s3 + i) && *(s1 + i))
+		i++;
+	if (*(s3 + i))
+		return (*(s1 + i) - *(s3 + i));
+	else
+		return (0);
+}
+
